75e445ee11dc3e8288e12c8173132cdc9fbd74cb
DiegoLara-FlagerCraft <laramnbvo123@gmail.com>
2021-03-24 16:02:44 -0500

    PUNTO 2

---
+++ PUNTO 2.py
@@ -0,0 +1,8 @@
+print("Ingrese un numero entre 0 y 10 entero diferente de 0, para terminar el programa ingrese el 0")
+n = int(input())
+while n != 0:
+    for l in range(n , 20 , 1):
+        print(l)
+    print("Ingrese un numero entre 0 y 10 entero diferente de 0, para terminar el programa ingrese el 0")
+    n = int(input())
+
