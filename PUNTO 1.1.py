95e28bffb740bd9350ae1f468e61a38ba683136a
DiegoLara-FlagerCraft <laramnbvo123@gmail.com>
2021-03-24 16:01:22 -0500

    PUNTO 1.1

---
+++ PUNTO 1.1.py
@@ -0,0 +1,35 @@
+print("EJERCICIO 1")
+for a in range(9):
+    print(a)
+
+print("EJERCICIO 2")
+for b in range(3, 13):
+    print(b)
+
+print("EJERCICIO 3")
+for c in range(3, 13, 2):
+    print(c)
+
+print("EJERCICIO 4")
+for d in range(11, 4):
+    print(d)
+
+print("EJERCICIO 5")
+for e in range(11, 4, -1):
+    print(e)
+
+print("EJERCICIO 6")
+for f in range(2, 10, 2):
+    print(f)
+
+print("EJERCICIO 7")
+for g in range(2, 10, 3):
+    print(g)
+
+print("EJERCICIO 8")
+for h in range(10, 5, -1):
+    print(h)
+
+print("EJERCICIO 9")
+for i in range(3, -1, -1):
+    print(i)
