933a265f60fc3b61d68e3025087d708ff943a23d
DiegoLara-FlagerCraft <laramnbvo123@gmail.com>
2021-04-07 17:48:51 -0500

    EJEMPLO CON CONTINUE

---
+++ EJEMPLO CONTINUE.py
@@ -0,0 +1,33 @@
+while True:
+    option = input("""Elije una fruta para tu desayuno:
+    1 - Manzanas
+    2 - Bananas
+    3 - Pera
+    4 - Uva
+    5 - Sandia
+    6 - Granola
+    0 - TERMINAR
+    """)
+    if option == "1":
+        print("Has seleccionado Manzanas")
+        continue
+    elif option == "2":
+        print("Has seleccionado Bananas")
+        continue
+    elif option == "3":
+        print("Has seleccionado Pera")
+        continue
+    elif option == "4":
+        print("Has seleccionado Uva")
+        continue
+    elif option == "5":
+        print("Has seleccionado Sandia")
+        continue
+    elif option == "6":
+        print("Has seleccionado Granola")
+        continue
+    elif option == "0":
+        break
+    else:
+        print("Debes seleccionar una opcion (1, 2 o 3)")
+print("Programa terminado, que disfrutes tu desayuno :)")
