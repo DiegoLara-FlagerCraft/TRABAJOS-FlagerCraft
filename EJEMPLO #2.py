b4cbfc73337d8e090066d7c08650735803131cc8
DiegoLara-FlagerCraft <laramnbvo123@gmail.com>
2021-04-07 17:47:51 -0500

    EJEMPLO 2

---
+++ EJEMPLO #2.py
@@ -0,0 +1,5 @@
+# 2.Mientras que año sea menor o igual a 2012, imprimir la frase "Informes del Año año".
+anio = 2010 
+while anio <= 2012: 
+    print ("Informes del Año", str(anio)) 
+    anio += 1
