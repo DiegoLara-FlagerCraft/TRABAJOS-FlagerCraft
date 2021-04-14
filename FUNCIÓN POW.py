ad32499749fb5f3e713374e8fb514500dd10487f
DiegoLara-FlagerCraft <laramnbvo123@gmail.com>
2021-04-14 16:05:14 -0500

    FUNCIÓN POW

---
+++ FUNCIÓN POW.py
@@ -0,0 +1,4 @@
+print("Vamos a calcular el resultado la elevacion de un numero a otro")
+n = float(input("Ingrese un numero que tome el rol de base: "))
+m = float(input("Ingrese un numero que tome el rol de exponente: "))
+print("El resultado de la elevacion es: ", pow(n, m))
