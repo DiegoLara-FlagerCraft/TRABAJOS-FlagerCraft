35e28f7ff650c5f869ab97c2942b6e258b321220
DiegoLara-FlagerCraft <laramnbvo123@gmail.com>
2021-04-14 16:04:57 -0500

    FUNCIÓN BOOL

---
+++ FUNCIÓN BOOL.py
@@ -0,0 +1,3 @@
+print("Evaluaremos si un numero es par o no. Si es par saldra como resultado True y si no False")
+n = int(input("Ingrese un numero entero para evaluar si es par: "))
+print(bool(n % 2 ==0))
